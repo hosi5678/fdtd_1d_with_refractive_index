917b646602aca68986b8a349856c900578e282bd
kenichi.hoshi <hosi5678@gmai.com>
2024-01-16 16:47:38 +0900

    ・CMakeLists.txtで変数${angular_frequency_num}の位置をadd_executableの  前に変更

--- fdtd1d_gaussian/src/setCoef1.c
+++ fdtd1d_gaussian/src/setCoef1.c
@@ -2,34 +2,21 @@
 #include <stdlib.h>
 
 #include "../../common_include/common_parameter.h"
+#include "../../common_include/checkAlloc1DDouble.h"
 #include "../../common_include/setCoef1.h"
 
 double *setCoef1(double *eps,double *sigma,int length){
 
-    double *coef;
+    double *coef1;
     double dt;
 
     dt=(dx/light_speed)*time_margin;
 
-    coef=(double *)calloc(length,sizeof(double));
-
-    if(NULL==coef){
-        printf("can't alloc error coef1.\n");
-        exit(1);
-    }
-
-    if(dt==dx/light_speed){
-        // printf("margin error.\n");
-        // exit(1);
-    }
+    coef1=checkAlloc1DDouble("coef1",length);
 
     for(int x=0;x<length;x++){
-        coef[x]=(2.0*eps[x]-sigma[x]*dt)/(2.0*eps[x]+sigma[x]*dt);
+        coef1[x]=(2.0*eps[x]-sigma[x]*dt)/(2.0*eps[x]+sigma[x]*dt);
     }
 
-    // for(int x=0;x<length;x++){
-    //     printf("coef1[%d]=%f\n",x,coef[x]);
-    // }
-
-    return coef;
+    return coef1;
 }
